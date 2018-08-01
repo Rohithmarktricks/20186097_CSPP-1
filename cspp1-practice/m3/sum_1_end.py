2d09338ee1e74819d07fbceed26081c43464931c
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-02 00:48:54 +0530

    sum of n integers from 1

---
+++ cspp1-practice/m3/sum_1_end.py
@@ -0,0 +1,11 @@
+'''
+@author : Rohithmarktricks
+This program prints the sum of n variable from 1
+'''
+SUM_ = 0
+END = int(input("Enter the end variable :"))
+i = 1
+while i <= END:
+    SUM_ += i
+    i += 1
+print(SUM_)
