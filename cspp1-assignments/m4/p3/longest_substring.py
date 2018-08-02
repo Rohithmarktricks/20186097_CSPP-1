58089baea1d56e00f890ebf9c49d051c3be70ac1
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-03 01:32:39 +0530

    " -> 0 of 5 passed. pylint: 7.86/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -11,11 +11,11 @@ def main():
     print(len(input_str))
     for i,letter in list(enumerate(input_str)):
         sub_str = input_str[i]
-            while i+1 < len(input_str) and input_str[i] < input_str[i+1]:
-                i += 1
-                sub_str += input_str[i]
-            if len(sub_str) > len(temp_str):
-                temp_str = sub_str
+        while i+1 < len(input_str) and input_str[i] < input_str[i+1]:
+            i += 1
+            sub_str += input_str[i]
+        if len(sub_str) > len(temp_str):
+            temp_str = sub_str
     print(temp_str)
 
 if __name__== "__main__":
