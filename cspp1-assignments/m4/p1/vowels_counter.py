04703d11ced860f74ae2ddd97e1e0c1433f9d053
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-02 22:36:56 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -1,10 +1,16 @@
 #Assume s is a string of lower case characters.
-
 #Write a program that counts up the number of vowels contained in the string s.
 #Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl',
 #your program should print:
 #Number of vowels: 5
+'''
+@author : Rohithmarktricks
+This programs counts the number of vowels in the string.
+'''
 def main():
+    '''
+    This main function looks for vowels in the string using for loop and if condition.
+    '''
     str_input = input()
     # the input string is in s
     # remove pass and start your code here
