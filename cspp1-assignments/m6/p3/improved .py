8757a3897db01d04fd303a260578ecd661714473
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-04 23:03:56 +0530

    " -> 6 of 7 passed. pylint: 10.00/10 "

---
+++ cspp1-assignments/m6/p3/improved .py
@@ -0,0 +1,24 @@
+'''
+Given a  number int_input, find the product of all the digits
+example:
+    input: 123
+    output: 6
+'''
+def main():
+    '''
+    Read any number from the input, store it in variable int_input.
+    '''
+    int_input = int(input())
+    digi_prod = 1
+    for i in range(len(str(abs(int_input)))):
+        del i
+        k = int_input%10
+        digi_prod = digi_prod * k
+        int_input = int_input // 10
+    if int_input < 0:
+        print('-'+str(digi_prod))
+    else:
+        print(digi_prod)
+
+if __name__ == "__main__":
+    main()
