42b7431f97bd14f93469b3de7595a6ce33239ab3
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-07 02:08:20 +0530

    " -> 8 of 10 passed. pylint: 5.00/10 "

--- cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
@@ -9,42 +9,53 @@ balance - the outstanding balance on the credit card
 annualInterestRate - annual interest rate as a decimal
 monthlyPaymentRate - minimum monthly payment rate as a decimal
 
-For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
+For each month, calculate statements on the monthly payment and remaining balance.
+At the end of 12 months, print out the remaining
 balance. Be sure to print out no more than two decimal digits of accuracy - so print
 
 Remaining balance: 813.41
 instead of
 Remaining balance: 813.4141998135
 
-So your program only prints out one thing: the remaining balance at the end of the year in the format:
+So your program only prints out one thing:
+the remaining balance at the end of the year in the format:
 Remaining balance: 4784.0
 
 A summary of the required math is found below:
 Monthly interest rate= (Annual interest rate) / 12.0
 Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
 Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+Updated balance each month = (Monthly unpaid balance) + 
+(Monthly interest rate x Monthly unpaid balance)
 @Author : Rohithmarktricks
 '''
 
 def paying_debt_off_in_a_year(balance_int, annual_interest_rate, monthly_payment_rate):
-	updated_balance_each_month = balance_int
-	monthly_interest_rate = annual_interest_rate/12.0
-	for i in range(1,13,1):
-		minimum_monthly_payment = monthly_payment_rate*updated_balance_each_month
-		monthly_unpaid_balance = updated_balance_each_month - minimum_monthly_payment
-		updated_balance_each_month = monthly_unpaid_balance+(monthly_interest_rate*monthly_unpaid_balance)
-	return updated_balance_each_month
+    '''
+    This function will calculate the balance after end of year.
+    '''
+    updated_balance_each_month = balance_int
+    monthly_interest_rate = annual_interest_rate/12.0
+    for i in range(1,13,1):
+        del i
+        minimum_monthly_payment = monthly_payment_rate*updated_balance_each_month
+        monthly_unpaid_balance = updated_balance_each_month - minimum_monthly_payment
+        updated_balance_each_month = monthly_unpaid_balance+(monthly_interest_rate*monthly_unpaid_balance)
+    return updated_balance_each_month
 
 
-	
+    
 
 def main():
-	data_int = input()
-	data_int = data_int.split(' ')
-	data_int = list(map(float, data_int))
-	print("Remaining balance: %.2f" %paying_debt_off_in_a_year(data_int[0],data_int[1],data_int[2]))
+    '''
+    This main function will take the data and prints the 
+    remaining amount to be paid.
+    '''
+    data_int = input()
+    data_int = data_int.split(' ')
+    data_int = list(map(float, data_int))
+    print("Remaining balance: %.2f" %paying_debt_off_in_a_year(data_int[0], data_int[1], data_int[2]))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
 
