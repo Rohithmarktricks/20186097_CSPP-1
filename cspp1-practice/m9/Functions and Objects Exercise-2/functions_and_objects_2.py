6c541e225f6a9419e35d13eb3ede3fd38de397d4
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-08 18:31:32 +0530

    Increment by 1

--- cspp1-practice/m9/Functions and Objects Exercise-2/functions_and_objects_2.py
+++ cspp1-practice/m9/Functions and Objects Exercise-2/functions_and_objects_2.py
@@ -1,17 +1,26 @@
-#Exercise : Function and Objects Exercise-2
-#Implement a function that converts the given testList = [1, -4, 8, -9] into [2, -3, 9, -8]
-
+'''
+Exercise : Function and Objects Exercise-2
+Implement a function that converts the given testList = [1, -4, 8, -9] into [2, -3, 9, -8]
+@Author :Rohithmarktricks
+'''
 
 def apply_to_each(L, f):
+	'''
+	Increment the elements of list by 1
+	'''
 	for i in range(len(L)):
 		L[i] = f(L[i])
 	print(L)
 def inc(x):
+	'''
+	Increment the value by 1
+	'''
 	return x+1
-    
-
 
 def main():
+	'''
+	Main Function
+	'''
     data = input()
     data = data.split()
     list1 = []
