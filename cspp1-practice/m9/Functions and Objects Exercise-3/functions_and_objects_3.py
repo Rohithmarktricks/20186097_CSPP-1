ccd6f5f4c67e4c5afe10bd2ab314d30858b4ec6e
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-08 18:36:10 +0530

    Square of each element in list!

--- cspp1-practice/m9/Functions and Objects Exercise-3/functions_and_objects_3.py
+++ cspp1-practice/m9/Functions and Objects Exercise-3/functions_and_objects_3.py
@@ -1,17 +1,27 @@
-#Exercise : Function and Objects Exercise-3
-#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 16, 64, 81]
-
+'''
+Exercise : Function and Objects Exercise-3
+Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 16, 64, 81]
+@Author : Rohithmarktricks
+'''
 
 def apply_to_each(L, f):
+    '''
+    To find the square of each element in the list!
+    '''
     for i in range(len(L)):
         L[i] = f(L[i])
     print(L) 
 
 def square(x):
+    '''
+    Method to find square!
+    '''
     return x**2
-    
-    
+
 def main():
+    '''
+    Main Function!
+    '''
     data = input()
     data = data.split()
     list1 = []
@@ -19,5 +29,5 @@ def main():
         list1.append(int(j))
     apply_to_each(list1, square)
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
