f611a031945122846f3b85137f248a9fcf57c5e7
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-08 18:20:15 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- cspp1-assignments/m9/p2/assignment2.py
+++ cspp1-assignments/m9/p2/assignment2.py
@@ -13,7 +13,6 @@ def get_guessed_word(secret_word, letters_guessed):
     returns: string, comprised of letters and underscores that represents
       what letters in secret_word have been guessed so far.
     '''
-    
     str1 = ""
     for i in secret_word:
         if i not in letters_guessed:
