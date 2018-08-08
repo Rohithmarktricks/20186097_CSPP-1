eeb150863e1b514de677458e5779c1eccdb90bea
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-08 18:26:32 +0530

    abs of list of elements!

--- cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
+++ cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -1,13 +1,21 @@
-#Exercise : Function and Objects Exercise-1
-#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+'''
+Exercise : Function and Objects Exercise-1
+Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+@Author : Rohithmarktricks
+'''
 
-
-def apply_to_each(L, f):
-	for i in range(len(L)):
-		L[i] = f(L[i])
-	print(L)
+def apply_to_each(l_int, f_int):
+    '''
+    Function to calculate abs of elements!
+    '''
+    for i in range(len(l_int)):
+        l_int[i] = f_int(l_int[i])
+    print(l_int)
 
 def main():
+    '''
+    Main function
+    '''
     data = input()
     data = data.split()
     list1 = []
