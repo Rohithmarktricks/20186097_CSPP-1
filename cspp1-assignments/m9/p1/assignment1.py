d0a5d8235b5e12dddc64f251c1c4705ec67b9639
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-08 17:53:44 +0530

    " -> 5 of 9 passed. pylint: 8.89/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -18,7 +18,8 @@ def is_word_guessed(secret_word, letters_guessed):
     for i in secret_word:
         if i in letters_guessed:
             return True
-    return False
+    
+        return False
 
    
 
