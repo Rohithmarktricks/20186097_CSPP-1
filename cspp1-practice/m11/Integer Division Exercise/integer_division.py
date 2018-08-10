67ba26b0b416dbb91449267b68493c95d937c4e3
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-10 16:28:26 +0530

    Division of integers using successive subtraction

---
+++ cspp1-practice/m11/Integer Division Exercise/integer_division.py
@@ -0,0 +1,29 @@
+'''
+Exercise: Integer Division Exercise
+Modify the code for integer_division so that this error does not occur.
+@author: Rohithmarktricks
+'''
+def integer_division(x_int, a_int):
+    """
+    x_int: a_int non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the integer division of x divided by a.
+    """
+    count = 0
+    while x_int >= a_int:
+        count += 1
+        x_int = x_int - a_int
+    return count
+
+def main():
+    '''
+    Main function that takes in integer
+    '''
+    data = input()
+    data = data.split()
+    print(integer_division(int(data[0]), int(data[1])))
+
+
+if __name__ == "__main__":
+    main()
