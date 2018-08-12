b6ee88c00343d8110a14d310eafdeb189dce20e4
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-12 14:35:18 +0530

    " -> 2 of 3 passed. pylint: -0.95/10 "

--- cspp1-assignments/m11/p3/assignment3.py
+++ cspp1-assignments/m11/p3/assignment3.py
@@ -7,7 +7,6 @@ Testing: Make sure the test_isValidWord tests pass. In addition, you will want t
 Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
 @author : Rohithmarktricks
 '''
-count = 0
 def isValidWord(word, hand, wordList):
     """
     Returns True if word is in the wordList and is entirely
@@ -19,13 +18,7 @@ def isValidWord(word, hand, wordList):
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
-    global count
-    print(hand)
-    print(wordList)
-    print(word)
-    print(word in wordList)
-    #print(c)
-    print(word)
+    count = 0
     if word in wordList:
         for i in list(word):
             if i in hand:
