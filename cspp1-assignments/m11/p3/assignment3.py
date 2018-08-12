eb9dcb4b23656ac9ef9bf3a60c9e524df03946d6
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-12 12:12:19 +0530

    " -> 0 of 3 passed. pylint: -0.42/10 "

--- cspp1-assignments/m11/p3/assignment3.py
+++ cspp1-assignments/m11/p3/assignment3.py
@@ -18,6 +18,7 @@ def isValidWord(word, hand, wordList):
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
+    print(wordList)
     count = 0
     if word in wordList:
         for i in word:
