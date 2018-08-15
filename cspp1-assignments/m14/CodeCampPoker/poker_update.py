0cc03e6c1ab54d938a6d6df88e79b946b880a212
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-15 17:02:05 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- cspp1-assignments/m14/CodeCampPoker/poker_update.py
+++ cspp1-assignments/m14/CodeCampPoker/poker_update.py
@@ -93,7 +93,7 @@ def is_three_a_kind(hand):
     #new_suit_list = sorted(suit_list)
     suit_list.sort()
     #a = list(set(new_suit_list))
-    for i in range(len(suit_list)-2)
+    for i in range(len(suit_list)-2):
     if new_suit_list[i] == new_suit_list[i+1] == new_suit_list[i+2] == new_suit_list[i+3]:
         return True
     # elif new_suit_list[1] == new_suit_list[2] == new_suit_list[3] == new_suit_list[4]:
