ed05cef33123e501201f2aece6a9cef08fceaa73
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-18 10:55:26 +0530

    " -> 0 of 1 passed. pylint: 9.77/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -74,12 +74,12 @@ def build_search_index(docs):
             for letter in new_clean_list:
                 if letter not in file_stop and letter not in '0123456789':
                     new_dict[letter] = new_dict.get(letter, 0)+1
-                    if i_ref not in search_index:
+                    if letter not in search_index:
                         search_index[letter] = [(i, new_dict[letter])]
                     else:
                     # new = [i_ref, new_dict[letter]]
                     # print(new)
-                        [search_index[letter],(i, new_dict[letter])]
+                        search_index[letter] = [search_index[letter],(i, new_dict[letter])]
     return search_index
 
 # helper function to print the search index
