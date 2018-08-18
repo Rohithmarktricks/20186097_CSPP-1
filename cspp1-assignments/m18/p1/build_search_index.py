ae759d73206c96a60c83828b964af15594927877
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-18 12:18:06 +0530

    " -> 0 of 1 passed. pylint: 9.77/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -79,8 +79,7 @@ def build_search_index(docs):
                 else:
                     if i_ref == search_index[letter][0]:
                         search_index[letter] = search_index[letter][1]+1
-                    else:
-                        search_index[letter].append((i_ref, new_dict[letter]))
+                    
     return search_index
 
 # helper function to print the search index
