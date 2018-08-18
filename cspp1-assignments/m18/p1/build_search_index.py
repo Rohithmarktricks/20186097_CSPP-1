2ff79c73d0bec325bc660e64d6dde5e5ceca1ab8
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-18 10:45:29 +0530

    " -> 0 of 1 passed. pylint: 9.53/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -68,19 +68,18 @@ def build_search_index(docs):
     for doc_id, doc in list(enumerate(docs)):
         doc_id_list.append(doc_id)
         doc_list.append(doc)
-        # new = zip(doc_id_list, doc_list)
-        # for i in range(len(new)):
-        #     new_doc = word_list(i[0][1])
-        #     i_ref = i[0][0]
-    for i,val in list(enumerate(doc_list)):
-        if val not in file_stop and val not in '0123456789':
-            new_dict[val] = new_dict.get(val, 0)+1
-            if doc_id_list[i] not in search_index:
-                search_index[val] = [(doc_id_list[i], new_dict[val])]
-            else:
-                # new = [i_ref, new_dict[letter]]
-                # print(new)
-                [search_index[val],(doc_id_list[i], new_dict[val])]
+    for i in zip(doc_id_list, doc_list):
+        new_doc = word_list(i[1])
+        i_ref = i[0]
+        for letter in new_doc:
+            if letter not in file_stop and letter not in '0123456789':
+                new_dict[letter] = new_dict.get(letter, 0)+1
+                if i_ref not in search_index:
+                    search_index[letter] = [(i_ref, new_dict[letter])]
+                else:
+                    # new = [i_ref, new_dict[letter]]
+                    # print(new)
+                    [search_index[letter],(i_ref, new_dict[letter])]
     return search_index
 
 # helper function to print the search index
