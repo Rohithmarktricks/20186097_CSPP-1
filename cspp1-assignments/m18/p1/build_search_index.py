ba1002b90f62a3f1cae6c94277f844dc0b708bb1
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-18 12:32:33 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -77,7 +77,7 @@ def build_search_index(docs):
                 if letter not in search_index:
                     search_index[letter] = [(i_ref, new_dict[letter])]
                 else:
-                    if i_ref == search_index[letter][0]:
+                    if i_ref in new_dict.keys():
                         search_index[letter] = search_index[letter][1]+1
                     else:
                         search_index[letter].append((i_ref, new_dict[letter]))
