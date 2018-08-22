5b49cd26457fad501d465e889770e7d4e956f93d
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-22 22:30:40 +0530

    Coordinates

--- cspp1-practice/m14/Classes and Objects - Coordinate Exercise/coordinate_exercise.py
+++ cspp1-practice/m14/Classes and Objects - Coordinate Exercise/coordinate_exercise.py
@@ -1,30 +1,49 @@
-# Exercise: coordinate
-
-# Consider the following code from the last lecture video:
+'''
+Exercise: coordinate
+Consider the following code from the last lecture video:
+'''
 
 class Coordinate(object):
-     def __init__(self, x, y):
-         self.x = x
-         self.y = y
+    '''
+    Class Coordinate
+    '''
+    def __init__(self, x_val, y_val):
+        '''
+        init constructor
+        '''
+        self.x_val = x_val
+        self.y_val = y_val
 
-     def getX(self):
-        # Getter method for a Coordinate object's x coordinate.
-        # Getter methods are better practice than just accessing an attribute directly
-        return self.x
+    def getx(self):
+        '''
+        Getter method for a Coordinate object's x coordinate.
+        Getter methods are better practice than just accessing an attribute directly
+        '''
+        return self.x_val
 
-	def getY(self):
-		# Getter method for a Coordinate object's y coordinate
-		return self.y
+    def gety(self):
+        '''
+        Getter method for a Coordinate object's y coordinate
+        '''
+        return self.y_val
 
-        
-	def __str__(self):
-		return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+    def __str__(self):
+        '''
+        str method
+        '''
+        return '<' + str(self.getx()) + ',' + str(self.gety()) + '>'
 
-	def __eq__(self, other):
-		return self.x == other.x and self.y == other.y
+    def __eq__(self, other):
+        '''
+        eq method
+        '''
+        return self.x_val == other.x_val and self.y_val == other.y_val
 
-	def __repr__(self):
-		return "(%s, %s)"%(self.x, self.y)
+    def __repr__(self):
+        '''
+        repr methods
+        '''
+        return "(%s, %s)"%(self.x_val, self.y_val)
 
 
 # Your task is to define the following two methods for the Coordinate class:
@@ -40,11 +59,14 @@ class Coordinate(object):
 
 
 def main():
-	data = raw_input()
-	data = data.split(' ')
-	data = list(map(int, data))
-	print(Coordinate(data[0],data[1]) == Coordinate(data[2],data[3]))
-	print(Coordinate(data[4],data[5]).__repr__())
+    '''
+    Main method
+    '''
+    data = input()
+    data = data.split(' ')
+    data = list(map(int, data))
+    print(Coordinate(data[0], data[1]) == Coordinate(data[2], data[3]))
+    print(Coordinate(data[4], data[5]).__repr__())
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
