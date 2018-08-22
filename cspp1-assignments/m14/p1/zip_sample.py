f86bcf66fa3f4053032d787249ade4ad057e22fb
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-22 18:47:27 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

---
+++ cspp1-assignments/m14/p1/zip_sample.py
@@ -0,0 +1,5 @@
+l = [i for i in range(6)]
+m = [j for j in range(6)]
+for k in list(zip(l,m)):
+	print(k)
+print(k[0])
---
+++ cspp1-assignments/m14/ps-6/pattern.py
@@ -0,0 +1,5 @@
+lim = int(input(""))
+for i in range(1,lim+1):
+	for j in range(i):
+		print(i, end = "")
+	print("")
