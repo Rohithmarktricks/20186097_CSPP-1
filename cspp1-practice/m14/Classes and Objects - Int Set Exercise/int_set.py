d6994d585674db6be7f7bf62160a57978c00829d
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-22 22:29:51 +0530

    Intset

--- cspp1-practice/m14/Classes and Objects - Int Set Exercise/int_set.py
+++ cspp1-practice/m14/Classes and Objects - Int Set Exercise/int_set.py
@@ -1,9 +1,10 @@
-# Exercise: int set
+'''
+Exercise: int set
+Consider the following code from the last lecture video:
+'''
 
-# Consider the following code from the last lecture video:
-
-class intSet(object):
-    """An intSet is a set of integers
+class Intset(object):
+    """An Intset is a set of integers
     The value is represented by a list of ints, self.vals.
     Each int in the set occurs in self.vals exactly once."""
 
@@ -11,38 +12,43 @@ class intSet(object):
         """Create an empty set of integers"""
         self.vals = []
 
-    def insert(self, e):
-        """Assumes e is an integer and inserts e into self""" 
-        if not e in self.vals:
-            self.vals.append(e)
+    def insert(self, value_):
+        """Assumes e is an integer and inserts e into self"""
+        if not value_ in self.vals:
+            self.vals.append(value_)
 
-    def member(self, e):
+    def member(self, value_):
         """Assumes e is an integer
            Returns True if e is in self, and False otherwise"""
-        return e in self.vals
+        return value_ in self.vals
 
-    def remove(self, e):
+    def remove(self, value_):
         """Assumes e is an integer and removes e from self
            Raises ValueError if e is not in self"""
         try:
-            self.vals.remove(e)
+            self.vals.remove(value_)
         except:
-            raise ValueError(str(e) + ' not found')
+            raise ValueError(str(value_) + ' not found')
 
     def __str__(self):
         """Returns a string representation of self"""
         self.vals.sort()
-        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+        return '{' + ','.join([str(value_) for value_ in self.vals]) + '}'
 
     def __len__(self):
         return len(self.vals)
-    
-        
-# Your task is to define the following two methods for the intSet class:
-#   1. Define an intersect method that returns a new intSet containing elements that
+
+    def intersect(self, other):
+        '''
+        intersect method
+        '''
+        return [value for value in self.vals if value in other.vals]
+
+# Your task is to define the following two methods for the Intset class:
+#   1. Define an intersect method that returns a new Intset containing elements that
 #   appear in both sets. In other words,
 #      s1.intersect(s2)
-#      would return a new intSet of integers that appear
+#      would return a new Intset of integers that appear
 #       in both s1 and s2. Think carefully - what should happen if s1 and s2 have no
 #      elements in common?
 
@@ -53,19 +59,22 @@ class intSet(object):
 #   what you'll need to solve this problem.
 
 
-        
-def main():
-    setA = intSet()
-    setB = intSet()
-    data=raw_input()
-    data1=raw_input()
-    l1=data.split()
-    l2=data1.split()
-    for i in l1:
-        setA.insert(int(i))
-    for j in l2:
-        setB.insert(int(j))
-    print setA.intersect(setB)
 
-if __name__== "__main__":
+def main():
+    '''
+    Main method
+    '''
+    seta = Intset()
+    setb = Intset()
+    data = input()
+    data1 = input()
+    list_1 = data.split()
+    list_2 = data1.split()
+    for i in list_1:
+        seta.insert(int(i))
+    for j in list_2:
+        setb.insert(int(j))
+    print(seta.intersect(setb))
+
+if __name__ == "__main__":
     main()
