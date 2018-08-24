b4ad92d0c8cbe7678ca270dc210e5d34aec66cac
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-24 10:17:18 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- TicTocToe/tictactoe.py
+++ TicTocToe/tictactoe.py
@@ -3,67 +3,67 @@ COLUMNS = 3
 
 
 def read_tictactoe():
-	tictactoe = []
-	for _ in range(ROWS):
-        list_matrix_row = input().split()
+    tictactoe = []
+    for _ in range(ROWS):
+        list_matrix_row = input().split(" ")
         for i in list_matrix_row:
-        	if i in 'x.o':
-            	tictactoe.append([i  for i in list_matrix_row])
-        	else:
-            	print("Invalid input")
-            	return None
+            if i in 'x.o':
+                tictactoe.append([i  for i in list_matrix_row])
+            else:
+                print("Invalid input")
+                return None
     return tictactoe
 
 def is_valid_game():
-	x_ns = tictactoe.count('x')
-	o_ns = tictactoe.count('o')
-	if x_ns or o_ns > 5:
-		print("Invalid Game")
-		return None
-	else:
-		return True
+    x_ns = tictactoe.count('x')
+    o_ns = tictactoe.count('o')
+    if x_ns or o_ns > 5:
+        print("Invalid Game")
+        return None
+    else:
+        return True
 
 def check_x():
-	new = zip(*tictactoe)
-	for row in tictactoe:
-		if row[0] == 'x' and len(set(row)) == 1 :
-			return True
-		elif:
-			for row in new:
-				if row[0] =='x' and len(set(row)) == 1:
-					return True
-				elif:
-					if (tictactoe[0][0] == tictactoe[1][1] == tictactoe[2][2] == 'x') or\
-						(tictactoe[0][2] == tictactoe[1][1] == tictactoe[2][0] == 'x'):
-						return True
-					else:
-						return False
+    new = zip(*tictactoe)
+    for row in tictactoe:
+        if row[0] == 'x' and len(set(row)) == 1 :
+            return True
+        elif:
+            for row in new:
+                if row[0] =='x' and len(set(row)) == 1:
+                    return True
+                elif:
+                    if (tictactoe[0][0] == tictactoe[1][1] == tictactoe[2][2] == 'x') or\
+                        (tictactoe[0][2] == tictactoe[1][1] == tictactoe[2][0] == 'x'):
+                        return True
+                    else:
+                        return False
 
 def check_o():
-	new = zip(*tictactoe)
-	for row in tictactoe:
-		if row[0] == 'o' and len(set(row)) == 1 :
-			return True
-		elif:
-			for row in new:
-				if row[0] =='o' and len(set(row)) == 1:
-					return True
-				elif:
-					if (tictactoe[0][0] == tictactoe[1][1] == tictactoe[2][2] == 'o') or\
-						(tictactoe[0][2] == tictactoe[1][1] == tictactoe[2][0] == 'o'):
-						return True
-					else:
-						return False
-								
-	
+    new = zip(*tictactoe)
+    for row in tictactoe:
+        if row[0] == 'o' and len(set(row)) == 1 :
+            return True
+        elif:
+            for row in new:
+                if row[0] =='o' and len(set(row)) == 1:
+                    return True
+                elif:
+                    if (tictactoe[0][0] == tictactoe[1][1] == tictactoe[2][2] == 'o') or\
+                        (tictactoe[0][2] == tictactoe[1][1] == tictactoe[2][0] == 'o'):
+                        return True
+                    else:
+                        return False
+                                
+    
 def main():
-	test = read_tictactoe()
-	if is_valid_game(test):
-		if check_x(test):
-			print('x')
-		elif check_o(test):
-			print('o')
-	else:
-		exit()
+    test = read_tictactoe()
+    if is_valid_game(test):
+        if check_x(test):
+            print('x')
+        elif check_o(test):
+            print('o')
+    else:
+        exit()
 
 
