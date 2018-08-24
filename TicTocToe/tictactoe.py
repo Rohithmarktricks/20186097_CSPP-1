7ff01942edc33d731b6bf2ced5f8ad301862fde4
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-24 11:26:28 +0530

    " -> 14 of 14 passed. pylint: 10.00/10 "

--- TicTocToe/tictactoe.py
+++ TicTocToe/tictactoe.py
@@ -49,7 +49,7 @@ def check_variables(check_test, check_variable):
         (check_test[0][2] == check_test[1][1] == check_test[2][0] == check_variable):
         return True
     return False
-    
+
 def main():
     '''
     Main function
