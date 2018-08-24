b7072205d5b5a87a0461baecf352a98db7651cf8
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-24 16:25:41 +0530

    " -> 14 of 14 passed. pylint: 10.00/10 "

--- cspp1-assignments/CodeCampMatrixOperations/fun.py
+++ cspp1-assignments/CodeCampMatrixOperations/fun.py
@@ -1,13 +1,18 @@
-s = [[1,2,3],[4,5,6],[7,8,9]]
-print(zip(*s))
+# s = [[1,2,3],[4,5,6],[7,8,9]]
+# print(zip(*s))
 #for y in zip(*s):
 #	print(y)
 # a = [1,2,3]
 # b = [4,5,6]
 # for i in zip(a,b):
 # 	print(i)
-a = [[1,2,2],[2,2,3]]
-new = 0
-for row in a:
-	new += row.count(2)
-print(new)
+# a = [[1,2,2],[2,2,3]]
+# new = 0
+# for row in a:
+# 	new += row.count(2)
+# print(new)
+# dict_ = {'a':1, 'b':2, 'c':3}
+# print(zip(*dict_))
+# for i, j in dict_.items():
+# 	print(i,j)
+sample = 
