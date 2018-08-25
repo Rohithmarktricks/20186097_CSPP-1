a572a77eb5c4ce764c72978081b240262706665c
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-25 10:49:40 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -2,12 +2,22 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
+adict = {}
 def tokenize(string):
-    pass
+    for word in string:
+    	if word not in adict:
+    		adict[word] = 1
+		else:
+			adict[word] += 1
+	return adict
             
 def main():
-    pass
-
+	'''
+	,Main function
+	'''
+	num = int(input())
+	for i in num:
+		new = tokenize(input())
+	print(new)
 if __name__ == '__main__':
     main()
