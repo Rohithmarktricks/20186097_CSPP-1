1abd727e31a9e58dccbf2083cc642f71a564bf88
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-25 13:42:57 +0530

    " -> 4 of 26 passed. pylint: 9.70/10 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -33,50 +33,50 @@ def check_column(sudoku):
                 return False
         return False
 
-def check_matrix(sudoku):
-    '''
-    to check the 3x3 grid
-    '''
-    new = np.array(sudoku)
-    a_arr = new[0].reshape((3, 3))
-    b_arr = new[1].reshape((3, 3))
-    c_arr = new[2].reshape((3, 3))
-    d_arr = new[3].reshape((3, 3))
-    e_arr = new[4].reshape((3, 3))
-    f_arr = new[5].reshape((3, 3))
-    g_arr = new[6].reshape((3, 3))
-    h_arr = new[7].reshape((3, 3))
-    i_arr = new[8].reshape((3, 3))
+# def check_matrix(sudoku):
+#     '''
+#     to check the 3x3 grid
+#     '''
+#     new = np.array(sudoku)
+#     a_arr = new[0].reshape((3, 3))
+#     b_arr = new[1].reshape((3, 3))
+#     c_arr = new[2].reshape((3, 3))
+#     d_arr = new[3].reshape((3, 3))
+#     e_arr = new[4].reshape((3, 3))
+#     f_arr = new[5].reshape((3, 3))
+#     g_arr = new[6].reshape((3, 3))
+#     h_arr = new[7].reshape((3, 3))
+#     i_arr = new[8].reshape((3, 3))
 
-    final = np.concatenate((a_arr[0], b_arr[0], c_arr[0]))
-    final2 = np.concatenate((a_arr[1], b_arr[1], c_arr[1]))
-    final3 = np.concatenate((a_arr[2], b_arr[2], c_arr[2]))
-    final4 = np.concatenate((d_arr[0], e_arr[0], f_arr[0]))
-    final5 = np.concatenate((d_arr[1], e_arr[1], f_arr[1]))
-    final6 = np.concatenate((d_arr[2], e_arr[2], f_arr[2]))
-    final7 = np.concatenate((g_arr[0], h_arr[0], i_arr[0]))
-    final8 = np.concatenate((g_arr[1], h_arr[1], i_arr[1]))
-    final9 = np.concatenate((g_arr[2], h_arr[2], i_arr[2]))
+#     final = np.concatenate((a_arr[0], b_arr[0], c_arr[0]))
+#     final2 = np.concatenate((a_arr[1], b_arr[1], c_arr[1]))
+#     final3 = np.concatenate((a_arr[2], b_arr[2], c_arr[2]))
+#     final4 = np.concatenate((d_arr[0], e_arr[0], f_arr[0]))
+#     final5 = np.concatenate((d_arr[1], e_arr[1], f_arr[1]))
+#     final6 = np.concatenate((d_arr[2], e_arr[2], f_arr[2]))
+#     final7 = np.concatenate((g_arr[0], h_arr[0], i_arr[0]))
+#     final8 = np.concatenate((g_arr[1], h_arr[1], i_arr[1]))
+#     final9 = np.concatenate((g_arr[2], h_arr[2], i_arr[2]))
 
-    if len(set(final)) == 9:
-        if len(set(final2)) == 9:
-            if len(set(final3)) == 9:
-                if len(set(final4)) == 9:
-                    if len(set(final5)) == 9:
-                        if len(set(final6)) == 9:
-                            if len(set(final7)) == 9:
-                                if len(set(final8)) == 9:
-                                    if len(set(final9)) == 9:
-                                        return True
-                                    return False
-                                return False
-                            return False
-                        return False
-                    return False
-                return False
-            return False
-        return False
-    return False
+#     if len(set(final)) == 9:
+#         if len(set(final2)) == 9:
+#             if len(set(final3)) == 9:
+#                 if len(set(final4)) == 9:
+#                     if len(set(final5)) == 9:
+#                         if len(set(final6)) == 9:
+#                             if len(set(final7)) == 9:
+#                                 if len(set(final8)) == 9:
+#                                     if len(set(final9)) == 9:
+#                                         return True
+#                                     return False
+#                                 return False
+#                             return False
+#                         return False
+#                     return False
+#                 return False
+#             return False
+#         return False
+#     return False
 
 def check_sudoku(sudoku):
     '''
@@ -103,8 +103,7 @@ def main():
         del i
         # read a line, split it on SPACE and append row to list
         row = input().split(' ')
-        for i in row:
-            sudoku.append(int(i))
+        sudoku.append([int(i) for i in row])
     # call solution function and print result to console
     print(check_sudoku(sudoku))
 
