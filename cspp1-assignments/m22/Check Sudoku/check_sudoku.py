196b14f443cf8816205013a54260b5a272ee81a0
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-25 12:39:52 +0530

    " -> 22 of 26 passed. pylint: 3.43/10 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -79,7 +79,7 @@ def check_sudoku(sudoku):
             if check_matrix(sudoku):
                 return False
             return True
-        return True
+        return False
     return False
 
 
